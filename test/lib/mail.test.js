806e707830badab12d64964cb022f3721c6775ac
Kyle Partridge <partkyle@gmail.com>
2012-01-09 15:46:34 -0800

    good start for tests

--- test/lib/mail.test.js
+++ test/lib/mail.test.js
@@ -1,7 +1,66 @@
 var Mail = require('../../lib/mail').Mail;
+var should = require('should');
+
+var text_params = {
+    username: 'kylep',
+    password: 'testing',
+    to: 'kyle.partridge@sendgrid.com',
+    from: 'kyle.partridge@sendgrid.com',
+    subject: 'Subject',
+    text: 'This is an email.'
+};
+
+var html_params = {
+    username: 'kylep',
+    password: 'testing',
+    to: 'kyle.partridge@sendgrid.com',
+    from: 'kyle.partridge@sendgrid.com',
+    subject: 'Subject',
+    html: '<b>This is an email.</b>'
+};
+
 describe('Mail', function () {
-    it("should be able to send mail", function () {
-        var mail = new Mail();
-        mail.send().should.be.true;
+    describe('statically', function() {
+        it('should be able to send text messages', function() {
+            Mail.send(text_params, function(success, message) {
+                if (!success) should.fail(message);
+            });
+        });
+
+        it('should be able to send html messages', function() {
+            Mail.send(html_params, function(success, message) {
+                if (!success) should.fail(message);
+            });
+        });
+    });
+
+    describe('instance', function() {
+        it('should be able to send text messages', function() {
+            var mail = new Mail(text_params);
+            mail.send(function(success, message) {
+                if (!success) should.fail(message);
+            });
+        });
+
+        it('should be able to send html messages', function() {
+            var mail = new Mail(html_params);
+            mail.send(function(success, message) {
+                if (!success) should.fail(message);
+            });
+        });
+    });
+
+    describe('validation', function() {
+        it('should invalidate when there are no parameters', function() {
+            var mail = new Mail();
+
+            mail.validate().should.be.false;
+        });
+
+        it('should return true when the mail is valid', function() {
+            var mail = new Mail(text_params);
+
+            mail.validate().should.be.true;
+        });
     });
 });
