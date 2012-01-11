9d000ec63c2fdca51dd3d426aa5701694aeab195
Kyle Partridge <partkyle@gmail.com>
2012-01-10 16:22:37 -0800

    added tests for exporting

--- test/index.test.js
+++ test/index.test.js
@@ -2,4 +2,10 @@ describe("index.js", function () {
   it("should exist", function () {
     require('../index').should.not.be.undefined;
   });
+  it('should export the SendGrid object', function() {
+    require('../index').SendGrid.should.not.be.undefined;
+  });
+  it('should export the Email object', function() {
+    require('../index').Email.should.not.be.undefined;
+  });
 });
