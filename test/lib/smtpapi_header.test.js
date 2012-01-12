fb99165efbf1746bbb39a78a8fe19f70fd265d53
Kyle Partridge <partkyle@gmail.com>
2012-01-11 18:19:36 -0800

    removed the data param

--- test/lib/smtpapi_header.test.js
+++ test/lib/smtpapi_header.test.js
@@ -9,23 +9,23 @@ describe('SmtpapiHeader', function() {
   describe('recipients', function() {
     it('should allow multiple to addresses', function() {
       header.addTo('kyle.partridge@sendgrid.com');
-      header.data.to.should.eql(['kyle.partridge@sendgrid.com']);
+      header.to.should.eql(['kyle.partridge@sendgrid.com']);
       header.addTo('david.tomberlin@sendgrid.com');
-      header.data.to.should.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
+      header.to.should.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
     });
 
     it('should allow multiple cc addresses', function() {
       header.addCc('kyle.partridge@sendgrid.com');
-      header.data.cc.should.eql(['kyle.partridge@sendgrid.com']);
+      header.cc.should.eql(['kyle.partridge@sendgrid.com']);
       header.addCc('david.tomberlin@sendgrid.com');
-      header.data.cc.should.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
+      header.cc.should.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
     });
 
     it('should allow multiple bcc addresses', function() {
       header.addBcc('kyle.partridge@sendgrid.com');
-      header.data.bcc.should.eql(['kyle.partridge@sendgrid.com']);
+      header.bcc.should.eql(['kyle.partridge@sendgrid.com']);
       header.addBcc('david.tomberlin@sendgrid.com');
-      header.data.bcc.should.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
+      header.bcc.should.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
     });
   });
 
@@ -33,7 +33,7 @@ describe('SmtpapiHeader', function() {
     var unique_args = { foo: 'bar', apple: 'sauce' };
     it('should allow setting unique args', function() {
       header.setUniqueArgs(unique_args);
-      header.data.unique_args.should.eql(unique_args);
+      header.unique_args.should.eql(unique_args);
     });
 
     it('should allow adding unique_args one at a time', function() {
@@ -42,17 +42,17 @@ describe('SmtpapiHeader', function() {
         args[key] = unique_args[key];
         header.addUniqueArgs(args);
       }
-      header.data.unique_args.should.eql(unique_args);
+      header.unique_args.should.eql(unique_args);
       header.addUniqueArgs({secret_test: 'rawr'});
-      header.data.unique_args.secret_test.should.eql('rawr');
+      header.unique_args.secret_test.should.eql('rawr');
     });
 
     it('should overwrite unique_args when calling the method twice', function() {
       header.setUniqueArgs(unique_args);
-      header.data.unique_args.should.eql(unique_args);
+      header.unique_args.should.eql(unique_args);
       header.addUniqueArgs({apple: 'pie'});
-      header.data.unique_args.should.not.eql(unique_args);
-      header.data.unique_args.apple.should.eql('pie');
+      header.unique_args.should.not.eql(unique_args);
+      header.unique_args.apple.should.eql('pie');
     });
   });
 
@@ -61,19 +61,19 @@ describe('SmtpapiHeader', function() {
 
     it('should allow setting a single category', function() {
       header.setCategory(categories[0]);
-      header.data.category.should.equal(categories[0]);
+      header.category.should.equal(categories[0]);
     });
 
     it('should allow adding categories one at a time', function() {
       header.addCategory(categories[0]);
       header.addCategory(categories[1]);
-      header.data.category.should.eql(categories);
+      header.category.should.eql(categories);
     });
 
     it('should allow setting categories', function() {
       var categories = ['azure', 'dreams'];
       header.setCategory(categories);
-      header.data.category.should.eql(categories);
+      header.category.should.eql(categories);
     });
   });
 
@@ -81,7 +81,7 @@ describe('SmtpapiHeader', function() {
     it('should be able to set basic filters', function() {
       header.addFilterSetting('footer', 'enable', 1);
       header.addFilterSetting('footer', 'text/html', '<b>boo</b>');
-      header.data.filters.should.eql({
+      header.filters.should.eql({
         'footer': {
           'settings': {
             'enable': 1,
@@ -101,7 +101,7 @@ describe('SmtpapiHeader', function() {
         }
       }
       header.setFilterSetting(filters);
-      header.data.filters.should.eql(filters);
+      header.filters.should.eql(filters);
     });
   });
 });
