3f51a4169402b0e54703fa6f188ce808ad189e73
David Tomberlin <david.tomberlin@sendgrid.com>
2012-01-16 16:55:42 -0800

    converted test to chai

--- test/lib/smtpapi_header.test.js
+++ test/lib/smtpapi_header.test.js
@@ -8,16 +8,16 @@ describe('SmtpapiHeader', function() {
 
   it('should allow multiple to addresses', function() {
     header.addTo('kyle.partridge@sendgrid.com');
-    header.to.should.eql(['kyle.partridge@sendgrid.com']);
+    expect(header.to).to.eql(['kyle.partridge@sendgrid.com']);
     header.addTo('david.tomberlin@sendgrid.com');
-    header.to.should.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
+    expect(header.to).to.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
   });
 
   describe('unique_args', function() {
     var unique_args = { foo: 'bar', apple: 'sauce' };
     it('should allow setting unique args', function() {
       header.setUniqueArgs(unique_args);
-      header.unique_args.should.eql(unique_args);
+      expect(header.unique_args).to.eql(unique_args);
     });
 
     it('should allow adding unique_args one at a time', function() {
@@ -26,17 +26,17 @@ describe('SmtpapiHeader', function() {
         args[key] = unique_args[key];
         header.addUniqueArgs(args);
       }
-      header.unique_args.should.eql(unique_args);
+      expect(header.unique_args).to.eql(unique_args);
       header.addUniqueArgs({secret_test: 'rawr'});
-      header.unique_args.secret_test.should.eql('rawr');
+      expect(header.unique_args.secret_test).to.eql('rawr');
     });
 
     it('should overwrite when calling addUniqueArgs with a value that already exists', function() {
       header.addUniqueArgs(unique_args);
-      header.unique_args.should.eql(unique_args);
+      expect(header.unique_args).to.eql(unique_args);
       header.addUniqueArgs({apple: 'pie'});
-      header.unique_args.should.not.eql(unique_args);
-      header.unique_args.apple.should.eql('pie');
+      expect(header.unique_args).not.to.eql(unique_args);
+      expect(header.unique_args.apple).to.eql('pie');
     });
   });
 
@@ -45,19 +45,19 @@ describe('SmtpapiHeader', function() {
 
     it('should allow setting a single category', function() {
       header.setCategory(categories[0]);
-      header.category.should.equal(categories[0]);
+      expect(header.category).to.equal(categories[0]);
     });
 
     it('should allow adding categories one at a time', function() {
       header.addCategory(categories[0]);
       header.addCategory(categories[1]);
-      header.category.should.eql(categories);
+      expect(header.category).to.eql(categories);
     });
 
     it('should allow setting categories', function() {
       var categories = ['azure', 'dreams'];
       header.setCategory(categories);
-      header.category.should.eql(categories);
+      expect(header.category).to.eql(categories);
     });
   });
 
@@ -65,7 +65,7 @@ describe('SmtpapiHeader', function() {
     var section_args = {'-sectionName1-': 'text 1', '-sectionName2-': 'text 2'};
     it('should allow setting a single section', function() {
       header.setSection(section_args);
-      header.section.should.eql(section_args);
+      expect(header.section).to.eql(section_args);
     });
 
     it('should allow adding section args one at a time', function() {
@@ -74,18 +74,18 @@ describe('SmtpapiHeader', function() {
         args[key] = section_args[key];
         header.addSection(args);
       }
-      header.section.should.eql(section_args);
+      expect(header.section).to.eql(section_args);
       header.addSection({cow: 'moo'});
-      header.section.should.not.eql(section_args);
-      header.section.cow.should.eql('moo');
+      expect(header.section).not.to.eql(section_args);
+      expect(header.section.cow).to.eql('moo');
     });
 
     it('should overwrite section when calling addSection with a value that already exists', function() {
       header.addSection(section_args);
-      header.section.should.eql(section_args);
+      expect(header.section).to.eql(section_args);
       header.addSection({'-sectionName1-': 'cows are nice'});
-      header.section.should.not.eql(section_args);
-      header.section['-sectionName1-'].should.eql('cows are nice');
+      expect(header.section).to.not.eql(section_args);
+      expect(header.section['-sectionName1-']).to.eql('cows are nice');
     });
   });
 
@@ -93,7 +93,7 @@ describe('SmtpapiHeader', function() {
     it('should be able to set basic filters', function() {
       header.addFilterSetting('footer', 'enable', 1);
       header.addFilterSetting('footer', 'text/html', '<b>boo</b>');
-      header.filters.should.eql({
+      expect(header.filters).to.eql({
         'footer': {
           'settings': {
             'enable': 1,
@@ -113,7 +113,7 @@ describe('SmtpapiHeader', function() {
         }
       }
       header.setFilterSetting(filters);
-      header.filters.should.eql(filters);
+      expect(header.filters).to.eql(filters);
     });
   });
 
@@ -126,43 +126,43 @@ describe('SmtpapiHeader', function() {
       header.addFilterSetting('footer', 'text/html', '<b>boo</b>');
 
       var parse = JSON.parse(header.toJson());
-      parse.to.should.eql(header.to);
-      parse.unique_args.should.eql(header.unique_args);
-      parse.filters.should.eql(header.filters);
+      expect(parse.to).to.eql(header.to);
+      expect(parse.unique_args).to.eql(header.unique_args);
+      expect(parse.filters).to.eql(header.filters);
     });
 
     it('should not include the "to" parameter when there are none', function() {
       header.setCategory('nothing');
       var json = header.toJson();
-      assert(_.isUndefined(JSON.parse(json).to), 'should be empty');
+      expect(_.isUndefined(JSON.parse(json).to)).to.be.true;
     });
 
     it('should not include the "sub" parameter when there are none', function() {
       header.setCategory('nothing');
       var json = header.toJson();
 
-      assert(_.isUndefined(JSON.parse(json).sub), 'should be empty');
+      expect(_.isUndefined(JSON.parse(json).sub)).to.be.true;
     });
 
     it('should not include the "unique_args" parameter when there are none', function() {
       header.setCategory('nothing');
       var json = header.toJson();
 
-      assert(_.isUndefined(JSON.parse(json).unique_args), 'should be empty');
+      expect(_.isUndefined(JSON.parse(json).unique_args)).to.be.true;
     });
 
     it('should not include the "category" parameter when there are none', function() {
       header.addUniqueArgs({food: 'bar'});
       var json = header.toJson();
 
-      assert(_.isUndefined(JSON.parse(json).category), 'should be empty');
+      expect(_.isUndefined(JSON.parse(json).category)).to.be.true;
     });
 
     it('should not include the "filters" parameter when there are none', function() {
       header.addUniqueArgs({food: 'bar'});
       var json = header.toJson();
 
-      assert(_.isUndefined(JSON.parse(json).filters), 'should be empty');
+      expect(_.isUndefined(JSON.parse(json).filters)).to.be.true;
     });
 
     it('should not remove any parameters on this object', function() {
@@ -175,9 +175,9 @@ describe('SmtpapiHeader', function() {
       // call the json method to test if it removed anything
       header.toJson();
 
-      header.to.should.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
-      header.unique_args.should.eql({foo: 'bar'});
-      header.filters.should.eql({
+      expect(header.to).to.eql(['kyle.partridge@sendgrid.com', 'david.tomberlin@sendgrid.com']);
+      expect(header.unique_args).to.eql({foo: 'bar'});
+      expect(header.filters).to.eql({
         footer: {
           settings: {
             enable: 1,
