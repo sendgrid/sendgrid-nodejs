e862fa0e2b447fc054c817f0107398c0d2ace5ce
David Tomberlin <david.tomberlin@sendgrid.com>
2012-01-27 10:23:32 -0800

    all test passing for attachments

--- test/integration/attachments.test.js
+++ test/integration/attachments.test.js
@@ -7,39 +7,115 @@ describe('attachments', function(){
     sendgrid = new SendGrid(setup.api_user, setup.api_key);
   });
 
-  it('should be able to send files via web', function(done) {
-    var mail = new Email({
-      to: setup.single_to,
-      from: setup.from,
-      subject: '(Web) File attachments',
-      text: 'test of files',
-      files: {
-        'Logo.png': __dirname + '/../assets/logo.png',
-        'Secret.txt': __dirname + '/../assets/secret.txt'
-      }
+  describe('web', function() {
+    var mail;
+    beforeEach(function() {
+      mail = new Email({
+        to: setup.single_to,
+        from: setup.from,
+        text: 'test of web files'
+      });
     });
 
-    sendgrid.send(mail, function(success, message) {
-      expect(success).to.be.true;
-      done();
+    it('should be able to send files via path', function(done) {
+      var file = {
+        filename: 'logo.png',
+        path: __dirname + '/../assets/logo.png'
+      };
+
+      mail.subject = '(Web-path) File attachments';
+      mail.addFile(file);
+      console.log('now sending');
+      sendgrid.send(mail, function(success, message) {
+        console.log('message: %s', message);
+        expect(success).to.be.true;
+        done();
+      });
+    });
+
+    it('should be able to send files via url', function(done) {
+      var file = {
+        filename: 'icon.jpg',
+        url: 'http://i.imgur.com/2fDh8.jpg'
+      };
+
+      mail.subject = '(Web-url) File attachments';
+      mail.addFile(file);
+      sendgrid.send(mail, function(success, message) {
+        expect(success).to.be.true;
+        done();
+      });
+    });
+
+    it('should be able to send files via content', function(done) {
+      var file = {
+        filename: 'hello_snowman.txt',
+        content: new Buffer("Hello ☃, I hope you don't melt", 'utf-8'),
+        contentType: 'text/plain'
+      };
+
+      mail.subject = '(Web-content) File attachments';
+      mail.addFile(file);
+      sendgrid.send(mail, function(success, message) {
+        expect(success).to.be.true;
+        done();
+      });
     });
   });
 
-  it('should be able to send files via Smtp', function(done) {
-    var mail = new Email({
-      to: setup.single_to,
-      from: setup.from,
-      subject: '(Smtp) File attachments',
-      text: 'test of files',
-      files: {
-        'Logo.png': __dirname + '/../assets/logo.png',
-        'Secret.txt': __dirname + '/../assets/secret.txt'
-      }
+  describe('smtp', function() {
+    var mail;
+    beforeEach(function() {
+      mail = new Email({
+        to: setup.single_to,
+        from: setup.from,
+        text: 'test of smtp files'
+      });
     });
 
-    sendgrid.smtp(mail, function(success, message) {
-      expect(success).to.be.true;
-      done();
+    it('should be able to send files via path', function(done) {
+      var file = {
+        filename: 'logo.png',
+        path: __dirname + '/../assets/logo.png'
+      };
+
+      mail.subject = '(Smtp-path) File attachments';
+      mail.addFile(file);
+      sendgrid.smtp(mail, function(success, message) {
+        console.log('message: %s', message);
+        expect(success).to.be.true;
+        done();
+      });
+    });
+
+    it('should be able to send files via url', function(done) {
+      var file = {
+        filename: 'icon.jpg',
+        url: 'http://i.imgur.com/2fDh8.jpg'
+      };
+
+      mail.subject = '(Smtp-url) File attachments';
+      mail.addFile(file);
+      sendgrid.smtp(mail, function(success, message) {
+        expect(success).to.be.true;
+        done();
+      });
+    });
+
+    it('should be able to send files via content', function(done) {
+      var file = {
+        filename: 'hello_snowman.txt',
+        content: new Buffer("Hello ☃, I hope you don't melt", 'utf-8'),
+        contentType: 'text/plain'
+      };
+
+      mail.subject = '(Smtp-content) File attachments';
+      mail.addFile(file);
+      sendgrid.smtp(mail, function(success, message) {
+        expect(success).to.be.true;
+        done();
+      });
     });
   });
+
 });
