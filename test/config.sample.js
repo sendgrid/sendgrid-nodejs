a54209711468cd1c7b806744c7b4c8dba4b7abf6
Leigh Martell <leigh.martell@gmail.com>
2013-02-01 10:38:10 -0500

    Updated mocha to support the proxy tests

--- test/config.sample.js
+++ test/config.sample.js
@@ -3,7 +3,8 @@ var test_setup = {
   api_key: '<password/api_key>',
   single_to: '<your_email>',
   multi_to: ['<your_email>', '<another_email>'],
-  from: '<your_email>'
+  from: '<your_email>',
+  options: {} //Must be at least {} add your proxy options here to test if you have a proxy
 }
 module.exports = test_setup;
 
