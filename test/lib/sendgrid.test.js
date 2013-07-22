82e358a4da3a1c36ee64a1a8501bb337eec91112
scottmotte <scott@scottmotte.com>
2013-07-22 22:14:22 +0000

    Version bump 0.2.11

--- test/lib/sendgrid.test.js
+++ test/lib/sendgrid.test.js
@@ -25,7 +25,7 @@ describe('SendGrid', function () {
   });
 
   it('version should be set', function() {
-    expect(sendgrid.version).to.equal("0.2.10");
+    expect(sendgrid.version).to.equal("0.2.11");
   });
 
   describe('#send', function() {
