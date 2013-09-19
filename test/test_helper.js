7e5f993b929ba61a28d75c8cc97e944d799b3b5a
Leigh Martell <leigh@goinstant.com>
2013-09-19 00:11:12 -0300

    Fixed tests and options

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,10 @@
 process.env.NODE_ENV  = 'test';
 var dotenv            = require('dotenv')();
+global.http = require('http');
+var options = { web: { pool: global.http.globalAgent } };
 dotenv.load();
 
 global.API_USER       = process.env.API_USER || 'some_sendgrid_username';
 global.API_KEY        = process.env.API_KEY || 'some_sendgrid_password';
-global.sendgrid       = require('../lib/sendgrid')(API_USER, API_KEY);
+global.sendgrid       = require('../lib/sendgrid')(API_USER, API_KEY, options);
 global.expect         = require('chai').expect;
