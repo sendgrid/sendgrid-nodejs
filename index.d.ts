8003ad1173d23cd4ac7a8417df76c166312cd56e
Benjamin Pannell <admin@sierrasoftworks.com>
2016-09-29 23:54:06 +0100

    feat: Add support for promises to the TypeScript definitions file

--- index.d.ts
+++ index.d.ts
@@ -466,6 +466,7 @@ declare namespace SendGrid {
         emptyRequest(): SendGrid.Rest.Request;
 
         API(request: SendGrid.Rest.Request, callback: (response: SendGrid.Rest.Response) => void): void;
+        API(request: SendGrid.Rest.Request): PromiseLike<SendGrid.Rest.Response>;
     }
 }
 
