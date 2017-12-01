8c3694cc9562ca5ecc58b3f8a44c76fa67d6c7b3
uniOpifex <njhunit@gmail.com>
2017-12-01 17:07:00 -0500

    updated test2

--- packages/helpers/helpers/html-to-plain-text.js
+++ packages/helpers/helpers/html-to-plain-text.js
@@ -6,7 +6,7 @@
  */
 
 
-module.exports = convertHTML2PlainString(html) {
+module.exports = function convertHTML2PlainString(html) {
   let text = html.replace(/(<([^>]+)>)/g, " ");
   text = text.replace(/\s+/g,' ')
   return text
