06f5640bad175c5ba8070f51169c6d44b3aaa89d
Elmer Thomas <elmer@ThinkingSerious.com>
2017-12-15 17:08:21 -0800

    Update html-to-plain-text.js

--- packages/helpers/helpers/html-to-plain-text.js
+++ packages/helpers/helpers/html-to-plain-text.js
@@ -1,11 +1,8 @@
 'use strict';
 
-
 /**
- * Helper to convert an html to a plain text string
+ * Helper to convert an HTML string to a plain text string
  */
-
-
 module.exports = function convertHTML2PlainString(html) {
   let text = html.replace(/(<([^>]+)>)/g, "");
   text = text.replace(/\s+/g,' ')
