9c58a8086fa12693389f9360ab88a8923e319ba6
Sc0tticus <ssinger303@gmail.com>
2020-09-14 22:12:24 -0600

    Removed excess files

--- example.js
+++
@@ -1,7 +0,0 @@
-if ('text' in conflictingData && 'dynamicTemplateData' in conflictingData) {
-  console.log('text and dynamicTemplateData cannot both have values');
-} else if ('html' in conflictingData && 'dynamicTemplate' in conflictingData) {
-  console.log('html and dynamicTemplateData cannot both have values');
-} else {
-  console.log('The email can be sent without issue.');
-}
