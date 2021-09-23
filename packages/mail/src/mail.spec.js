05fba7d416834f55715446f59886c0e4dd087744
Subinoy Ghosh <subinoy.ghosh@smartserv.io>
2021-09-24 02:15:43 +0530

    Added test case coverage for replyToList functionality

--- packages/mail/src/mail.spec.js
+++ packages/mail/src/mail.spec.js
@@ -17,7 +17,7 @@ before(() => {
  * Default mock header
  */
 beforeEach(() => {
-  sgClient.setDefaultHeader('X-Mock', 200);
+  sgClient.setDefaultHeader('X-Mock', 202);
 });
 
 /**
@@ -38,12 +38,12 @@ describe('sgMail.send()', () => {
     return expect(sgMail.send()).to.eventually.be.rejectedWith(Error);
   });
 
-  it('should send a basic email', () => {
-    sgClient.setDefaultHeader('X-Mock', 201);
+  it('should send a basic email', async () => {
+    sgClient.setDefaultHeader('X-Mock', 202);
     return sgMail
       .send(data)
       .then(([response, body]) => {
-        expect(response.statusCode).to.equal(201);
+        expect(response.statusCode).to.equal(202);
       });
   });
 
@@ -53,13 +53,13 @@ describe('sgMail.send()', () => {
     }).to.throw(Error);
   });
 
-  it('should include custom headers to the request', () => {
-    sgClient.setDefaultHeader('X-Mock', 201);
+  it('should include custom headers to the request', async () => {
+    sgClient.setDefaultHeader('X-Mock', 202);
     const clientSpy = sinon.spy(sgClient, "request")
     return sgMail
       .send(Object.assign(data, { headers: { customHeader: "Custom Header Content" } }))
       .then(([response, body]) => {
-        expect(response.statusCode).to.equal(201);
+        expect(response.statusCode).to.equal(202);
         expect(clientSpy).to.have.been.calledWith(sinon.match({
           url: "/v3/mail/send",
           method: "POST",
@@ -68,10 +68,69 @@ describe('sgMail.send()', () => {
       });
   });
 
-  // it('should have multiple replyTo emails', () => {
-  //   sgClient.setDefaultHeader('X-Mock', 201);
-  //   const clientSpy = sinon.spy(sgClient, "request");
-  //   debugger;
-  // })
+  it('should send email with correct replyToList format', async () => {
+    sgClient.setDefaultHeader('X-Mock', 202);
+    data["replyToList"] = [
+      {
+        "name": "Test Team",
+        "email": "test@twilio.com"
+      },
+      {
+        "name": "Support Test Team",
+        "email": "support.test@twilio.com"
+      }
+    ];
+    return sgMail
+      .send(data)
+      .then(([response, body]) => {
+        expect(response.statusCode).to.equal(202);
+      });
+  });
+
+  it('should throw error with wrong replyToList format', async () => {
+    sgClient.setDefaultHeader('X-Mock', 202);
+    data["replyToList"] = {
+      "name": "Support Test Team",
+      "email": "support.test@twilio.com"
+    };
+    return expect(function() {
+      sgMail.send(data, false, {});
+    }).to.throw(Error);
+  });
+
+  it('should throw error if any record in replyToList is without email', async () => {
+    data["replyToList"] = [
+      {
+        "name": "Test Team",
+        "email": "test@twilio.com"
+      },
+      {
+        "name": "Support Test Team"
+      }
+    ];
+    return expect(function() {
+      sgMail.send(data, false, {});
+    }).to.throw(Error);
+  });
+
+  it('should throw error if both replyTo and replyToList are mentioned', async () => {
+    data["replyTo"] = {
+        "name": "Test Team234234",
+        "email": "test234235@twilio.com"
+      };
+    data["replyToList"] = [
+      {
+        "name": "Test Team",
+        "email": "test@twilio.com"
+      },
+      {
+        "name": "Support Test Team",
+        "email": "support.test@twilio.com"
+      }
+    ];
+    return expect(function() {
+      sgMail.send(data, false, {});
+    }).to.throw(Error);
+  });
 });
 
